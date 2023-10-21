3905865bd1258d3dc1930995aca7084661805d9c
SaraswatAditya <adityasaraswat92@gmail.com>
2023-10-21 23:18:28 +0530

    First Addition

---
+++ src/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer = () => (
+    <section className="footer  bg-dark text-white">
+      <p className="lead container">Contact us at Instagram</p>
+      <p className="lead container">You can react via email too</p>
+      <p className="lead container">NO COPYRIGHT</p>
+    </section>
+
+);    // () in these no return key word is required
+
+
+export default Footer;
